da8f12e503c533e319fe1f6e59a45bc08441d592
Nicholas Young <hi@secretfader.com>
2019-04-25 10:01:16 -0600

    New resume

---
+++ README.md
@@ -0,0 +1,14 @@
+# Resume
+
+[![CI Status
+Badge](https://gitlab.com/secretfader/resume/badges/master/build.svg)](https://gitlab.com/secretfader/resume)
+[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+[![Follow on Twitter](https://img.shields.io/twitter/follow/secretfader.svg?label=Follow&style=social)](https://twitter.com/secretfader)
+
+The resume of hacker and artist Nicholas Young (a.k.a. "Fader").
+
+## License
+
+Copyright 2019 Nicholas Young. All rights reserved.
+
+Markup and design may be used under the [3-Clause BSD License](LICENSE). However, republishing content without express permission is a violation of applicable laws.
---
+++ scripts/generate-pdf.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+const puppeteer = require('puppeteer');
+const http = require('http-server');
+
+async function createPDF(output) {
+  const server = http.createServer({
+    base: path.resolve(__dirname, '../preview'),
+  });
+
+  server.listen(7878); // Because Rust is awesome.
+
+  // Create a new instance of Chrome
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+
+  /// Instantiate a page within the browser
+  const page = await browser.newPage();
+
+  /// Load `public/index.html`
+  await page.goto('http://127.0.0.1:7878/', {
+    waitUntil: 'networkidle0',
+  });
+
+  /// Render to PDF
+  await page.pdf({
+    path: path.resolve(__dirname, '../public', output),
+    format: 'Letter',
+  });
+
+  /// Shutdown browser instance
+  await browser.close();
+  server.close();
+}
+
+createPDF('resume.pdf').catch(console.error);
