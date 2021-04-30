f3ec75e3a957cd5179cf60d93914217b8f9130bc
Nicholas Young <hi@secretfader.com>
2021-04-30 17:54:33 -0600

    chore(ci): use full path to zola binary

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-zola build -u $DEPLOY_PRIME_URL && \
-zola build -u http://127.0.0.1:7878 -o preview/ && \
+$HOME/bin/zola build -u $DEPLOY_PRIME_URL && \
+$HOME/bin/zola build -u http://127.0.0.1:7878 -o preview/ && \
 npm ci && npm run generate-pdf
