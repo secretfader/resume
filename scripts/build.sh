1e31e1254c96b230de3ad4046a31bfc907b00f22
Nicholas Young <hi@secretfader.com>
2021-05-01 12:33:15 -0600

    chore(ci): run build script in the default shell

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
-$HOME/bin/zola build -u $DEPLOY_PRIME_URL && \
-$HOME/bin/zola build -u http://127.0.0.1:7878 -o preview/ && \
+#!/usr/bin/env /bin/sh
+zola build -u $DEPLOY_PRIME_URL && \
+zola build -u http://127.0.0.1:7878 -o preview/ && \
 npm run generate-pdf
