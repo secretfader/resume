17d38bf872727995c1bff0e9110028752021f9cd
Nicholas Young <hi@secretfader.com>
2021-05-02 11:57:17 -0600

    chore: install s3-util in the CI environment

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,20 +3,27 @@
 # Terminate early on pipeline failure, since we use pipes below
 set -e
 
-# Retrieve the latest stable, extended release version from GitHub
-DOWNLOAD_URL=$(curl --silent -H 'Accept: application/json' \
+# Create a reusable filter chain
+RELEASE_FILTER="(select(.draft == false) | select(.prerelease == false)).assets[] | \
+    select(.name | contains(\"x86_64-unknown-linux-gnu\"))"
+
+# Retrieve the latest stable release of Zola from GitHub
+ZOLA_URL=$(curl --silent -H 'Accept: application/json' \
     https://api.github.com/repos/getzola/zola/releases | \
-    jq -r "(.[0] |
-    (select(.draft == false) | select(.prerelease == false)).assets[] |
-        select(.name | contains(\"x86_64-unknown-linux-gnu.tar.gz\"))
-    ).browser_download_url"
+    jq -r "(.[0] | $RELEASE_FILTER).browser_download_url"
+)
+
+S3UTIL_URL=$(curl --silent -H 'Accept: application/json' \
+    https://api.github.com/repos/ryankurte/s3-util/releases | \
+    jq -r "(.[0] | $RELEASE_FILTER).browser_download_url"
 )
 
 # Make a directory at $HOME/bin to contain downloaded binaries
 mkdir -p $HOME/bin
 
-# Download the Zola binary and extract the archive contents $HOME/bin
-curl -sSL $DOWNLOAD_URL | tar -xzf - -C $HOME/bin
+# Download binaries for Zola and S3 Util, extracting the archive contents to $HOME/bin
+curl -sSL $ZOLA_URL | tar -xzf - -C $HOME/bin
+curl -sSL $S3UTIL_URL | tar -xzf - -C $HOME/bin
 
 # Install npm dependencies
 npm ci
