2e710501c9f28e8c3f67ae8842e6a71c2df68ea9
Nicholas Young <hi@secretfader.com>
2021-05-02 14:52:35 -0600

    refactor: sandbox browser (#13)

--- scripts/generate-pdf.js
+++ scripts/generate-pdf.js
@@ -2,24 +2,25 @@ const path = require("path");
 const puppeteer = require("puppeteer");
 const http = require("http-server");
 
+const port = process.env.PORT ? parseInt(process.env.PORT) : 7878;
+
 async function createPDF(output) {
   const server = http.createServer({
     base: path.resolve(__dirname, "../preview"),
   });
 
-  server.listen(7878); // Because Rust is awesome.
+  server.listen(port); // Because Rust is awesome.
 
   // Create a new instance of Chrome
   const browser = await puppeteer.launch({
     headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
   /// Instantiate a page within the browser
   const page = await browser.newPage();
 
   /// Load `public/index.html`
-  await page.goto("http://127.0.0.1:7878/", {
+  await page.goto(`http://127.0.0.1:${port}/`, {
     waitUntil: "networkidle0",
   });
 
