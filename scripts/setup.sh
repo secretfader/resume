94623d8e74c5f59453ee5672d2d5ac9e3d24c6e1
Nicholas Young <hi@secretfader.com>
2021-05-04 07:46:49 -0600

    chore: use curl quiet shorthand

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,12 +8,12 @@ RELEASE_FILTER="(.[0] | (select(.draft == false) | select(.prerelease == false))
     select(.name | contains(\"x86_64-unknown-linux-gnu\"))).browser_download_url"
 
 # Retrieve the latest stable release of Zola from GitHub
-ZOLA_URL=$(curl --silent -H 'Accept: application/json' \
+ZOLA_URL=$(curl -sS -H 'Accept: application/json' \
     https://api.github.com/repos/getzola/zola/releases | \
     jq -r "$RELEASE_FILTER"
 )
 
-S3UTIL_URL=$(curl --silent -H 'Accept: application/json' \
+S3UTIL_URL=$(curl -sS -H 'Accept: application/json' \
     https://api.github.com/repos/ryankurte/s3-util/releases | \
     jq -r "$RELEASE_FILTER"
 )
