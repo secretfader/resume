a3cdbf2cdf2675582f188652a220442d8267b4c9
Nicholas Young <hi@secretfader.com>
2021-05-04 07:07:58 -0600

    chore: add default deploy url to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env /bin/sh
 
-# Terminate early on pipeline failure
-set -e
-
 # Extend $PATH to include locally-installed binaries
 PATH=$PATH:$HOME/bin
+PORT=7878
+DEPLOY_PRIME_URL="${DEPLOY_PRIME_URL:=https://resume.secretfader.com}"
 
 # Build site and generate printable PDF
 zola build -u $DEPLOY_PRIME_URL && \
-zola build -u http://127.0.0.1:7878 -o preview/ && \
+zola build -u http://127.0.0.1:$PORT -o preview/ && \
 npm run generate-pdf
