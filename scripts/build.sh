7e3cd7507752d38d9f4cbbd39bc8be89834994e3
Nicholas Young <hi@secretfader.com>
2021-05-22 16:16:04 -0600

    chore: send output of second build to `/dev/null`

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,6 @@ PORT=7878
 DEPLOY_PRIME_URL="${DEPLOY_PRIME_URL:=https://resume.secretfader.com}"
 
 # Build site and generate printable PDF
-zola build -u $DEPLOY_PRIME_URL && \
-zola build -u http://127.0.0.1:$PORT -o preview/ && \
+zola build -u $DEPLOY_PRIME_URL
+zola build -u http://127.0.0.1:$PORT -o preview/ &> /dev/null
 npm run generate-pdf
