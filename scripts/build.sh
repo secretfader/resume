1cffd9cb14872bd4939ee2dd562752d45a9cfebc
Nicholas Young <hi@secretfader.com>
2021-05-23 09:03:18 -0600

    chore: add public/ directory as main output

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,5 @@ ZOLA_ENVIRONMENT="${ZOLA_ENVIRONMENT:=production}"
 DEPLOY_PRIME_URL="${DEPLOY_PRIME_URL:=https://resume.secretfader.com}"
 
 # Build site and generate printable PDF
-zola build -u $DEPLOY_PRIME_URL
+zola build -u $DEPLOY_PRIME_URL -o public/
 zola build -u http://127.0.0.1:$PORT -o preview/ && npm run generate-pdf
